bc8ce7c32bde8b23ebd2d674607971d76ecc3f23
emilymacmahon <70771497+emilymacmahon@users.noreply.github.com>
2021-04-28 09:50:27 -0400

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+<title>JAX Marketing Solutions</title>
+
+<meta charset="utf-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" type="text/css" href="styles.css">
+
+</head>
+
+
+<body>
+    
+<div class="header">
+  <h1>JAX Marketing Solutions</h1>
+  <p>Looking to the Future</p>
+</div>
+
+    <ul>
+            <li><a class="active" href="#home">Home</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="careers.html">Careers</a></li>
+            <li><a href="team.html">Team</a></li>
+            <li><a href="contact.php">Contact</a></li>
+    </ul>
+
+
+
+
+</body>
+
+</html>
+
